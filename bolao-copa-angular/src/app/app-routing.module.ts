6a9a8eae4e0cbd31d31aa9ceede010230bb4f6fa
Vinicius Cardoso <vinic8670@gmail.com>
2022-10-26 00:38:44 -0300

    creating a new style for login page

--- bolao-copa-angular/src/app/app-routing.module.ts
+++ bolao-copa-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './header/login/login.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NavbarComponent },
+  { path: 'login',  component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
